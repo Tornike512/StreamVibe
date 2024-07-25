2c185a5e6a9fe8469aaf8a29907169e2057b4043
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-25 21:28:44 +0400

    Add: Header Style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,18 +4,48 @@ import bellIcon from "src/assets/bell-icon.png";
 
 export function Header() {
   return (
-    <header className="flex bg-slate-600">
-      <img src={streamVibeLogo} alt="StreamVibe Logo" />
-      <nav>
-        <a href="#">Home</a>
-        <a href="#">Movies & Shows</a>
-        <a href="#">Support</a>
-        <a href="#">Subscriptions</a>
-      </nav>
-      <figure>
-        <img src={searchIcon} alt="Search Logo" />
-        <img src={bellIcon} alt="Bell Logo" />
-      </figure>
+    <header className="h-[120px] flex justify-center bg-slate-600">
+      <div className="max-w-[1920px] px-[162px] w-full flex justify-between items-center">
+        <img
+          className="cursor-pointer"
+          src={streamVibeLogo}
+          alt="StreamVibe Logo"
+        />
+        <nav className="bg-[#000000] py-[20px] px-[10px] rounded-[10px]">
+          <a
+            className="text-[#BFBFBF] text-[18px] px-[14px] py-[14px] mr-[5px] hover:bg-[#262626] hover:rounded-[10px] hover:text-[#ffffff]"
+            href="#"
+          >
+            Home
+          </a>
+          <a
+            className="text-[#BFBFBF] text-[18px] px-[14px] py-[14px] mr-[5px] hover:bg-[#262626] hover:rounded-[10px] hover:text-[#ffffff]"
+            href="#"
+          >
+            Movies & Shows
+          </a>
+          <a
+            className="text-[#BFBFBF] text-[18px] px-[14px] py-[14px] mr-[5px] hover:bg-[#262626] hover:rounded-[10px] hover:text-[#ffffff]"
+            href="#"
+          >
+            Support
+          </a>
+          <a
+            className="text-[#BFBFBF] text-[18px] px-[14px] py-[14px] hover:bg-[#262626] hover:rounded-[10px] hover:text-[#ffffff]"
+            href="#"
+          >
+            Subscriptions
+          </a>
+        </nav>
+        <figure className="flex">
+          <img
+            className="mr-[30px] cursor-pointer"
+            src={searchIcon}
+            alt="Search Logo"
+          />
+          <img className="cursor-pointer" src={bellIcon} alt="Bell Logo" />
+        </figure>
+      </div>
     </header>
   );
 }
