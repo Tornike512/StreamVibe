3e2e1cb0d6414041d79f1a01bd80a2a90f1f0bd1
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-27 14:11:12 +0400

    Add: Home Header Section HTML

--- src/pages/HomePage/HomeHeader/HomeHeader.tsx
+++ src/pages/HomePage/HomeHeader/HomeHeader.tsx
@@ -1,5 +1,24 @@
 export function HomeHeader() {
-  return <section></section>;
+  return (
+    <section>
+      <img src="" alt="Header Movies Images" />
+      <div>
+        <h1>The Best Streaming Experience</h1>
+        <p>
+          StreamVibe is the best streaming experience for watching your favorite
+          movies and shows on demand, anytime, anywhere. With StreamVibe, you
+          can enjoy a wide variety of content, including the latest
+          blockbusters, classic movies, popular TV shows, and more. You can also
+          create your own watchlists, so you can easily find the content you
+          want to watch.
+        </p>
+        <button>
+          <img src="" alt="Start Watching Button Logo" />
+          Start Watching Now
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default HomeHeader;
