e0fcc2b18080c21faaf5c45b9f525d8637c277b4
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-27 16:57:37 +0400

    Add: Slider Style

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,11 +1,42 @@
+import moviesGrid from "src/assets/movies-grid.png";
+import rightArrow from "src/assets/right-arrow.png";
+
 export function Slider() {
   return (
-    <ul>
-      <li>
-        <img src="" alt="Movies Images" />
-        <figure>
-          <figcaption>Action</figcaption>
-          <img src="" alt="right arrow" />
+    <ul className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr] gap-x-[30px]">
+      <li className="p-[30px] bg-[#262626] w-full rounded-[12px] cursor-pointer flex flex-col">
+        <img src={moviesGrid} alt="Movies Images" />
+        <figure className="flex justify-between mt-[5px]">
+          <figcaption className="text-[22px] text-[#ffffff]">Action</figcaption>
+          <img src={rightArrow} alt="Right Arrow Icon" />
+        </figure>
+      </li>
+      <li className="p-[30px] bg-[#262626] w-full rounded-[12px] cursor-pointer flex flex-col">
+        <img src={moviesGrid} alt="Movies Images" />
+        <figure className="flex justify-between mt-[5px]">
+          <figcaption className="text-[22px] text-[#ffffff]">Action</figcaption>
+          <img src={rightArrow} alt="Right Arrow Icon" />
+        </figure>
+      </li>
+      <li className="p-[30px] bg-[#262626] w-full rounded-[12px] cursor-pointer flex flex-col">
+        <img src={moviesGrid} alt="Movies Images" />
+        <figure className="flex justify-between mt-[5px]">
+          <figcaption className="text-[22px] text-[#ffffff]">Action</figcaption>
+          <img src={rightArrow} alt="Right Arrow Icon" />
+        </figure>
+      </li>
+      <li className="p-[30px] bg-[#262626] w-full rounded-[12px] cursor-pointer flex flex-col">
+        <img src={moviesGrid} alt="Movies Images" />
+        <figure className="flex justify-between mt-[5px]">
+          <figcaption className="text-[22px] text-[#ffffff]">Action</figcaption>
+          <img src={rightArrow} alt="Right Arrow Icon" />
+        </figure>
+      </li>
+      <li className="p-[30px] bg-[#262626] w-full rounded-[12px] cursor-pointer flex flex-col">
+        <img src={moviesGrid} alt="Movies Images" />
+        <figure className="flex justify-between mt-[5px]">
+          <figcaption className="text-[22px] text-[#ffffff]">Action</figcaption>
+          <img src={rightArrow} alt="Right Arrow Icon" />
         </figure>
       </li>
     </ul>
