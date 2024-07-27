7421e76b94cf26d937d09e973d86c9e6934defb4
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-27 16:22:02 +0400

    Add: Slider Buttons Component For Sliding Grid

---
+++ src/components/SliderButtons/SliderButtons.tsx
@@ -0,0 +1,19 @@
+import leftArrow from "src/assets/left-arrow.png";
+import rightArrow from "src/assets/right-arrow.png";
+
+export function SliderButtons() {
+  return (
+    <li className="w-[15%] bg-[#0F0F0F] rounded-[12px] flex justify-between items-center p-[16px]">
+      <button className="rounded-[8px] bg-[#1F1F1F] p-[19px] hover:bg-[#404040]">
+        <img src={leftArrow} alt="Left Arrow Icon" />
+      </button>
+      <span className="w-[20px] h-[5px] bg-[#E50000] ml-[10px]"></span>
+      <span className="w-[20px] h-[5px] bg-[#404040]"></span>
+      <span className="w-[20px] h-[5px] bg-[#404040]"></span>
+      <span className="w-[20px] h-[5px] bg-[#404040] mr-[10px]"></span>
+      <button className="rounded-[8px] bg-[#1F1F1F] p-[19px] hover:bg-[#404040]">
+        <img src={rightArrow} alt="Right Arrow Icon" />
+      </button>
+    </li>
+  );
+}
--- src/pages/HomePage/ExploreCategory/ExploreCategory.tsx
+++ src/pages/HomePage/ExploreCategory/ExploreCategory.tsx
@@ -1,26 +1,19 @@
+import { SliderButtons } from "src/components/SliderButtons";
+
 export function ExploreCategory() {
   return (
-    <section>
-      <ul>
+    <section className="max-w-[1920px] w-full">
+      <ul className="flex justify-between">
         <li>
-          <h2>Explore our wide variety of categories</h2>
-          <p>
+          <h2 className="text-[#ffffff] text-[38px] font-bold mb-[14px]">
+            Explore our wide variety of categories
+          </h2>
+          <p className="text-[18px] text-[#999999]">
             Whether you're looking for a comedy to make you laugh, a drama to
             make you think, or a documentary to learn something new
           </p>
         </li>
-        <li>
-          <button>
-            <img src="" alt="Left Arrow Icon" />
-          </button>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <button>
-            <img src="" alt="Right Arrow Icon" />
-          </button>
-        </li>
+        <SliderButtons />
       </ul>
       <ul>
         <li>
