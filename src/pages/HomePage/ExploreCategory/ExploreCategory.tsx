ee0ac8045799ff452c65870daad324ba9ae15cd7
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-27 15:38:50 +0400

    Add: Explore Categories Section HTML

--- src/pages/HomePage/ExploreCategory/ExploreCategory.tsx
+++ src/pages/HomePage/ExploreCategory/ExploreCategory.tsx
@@ -1,5 +1,38 @@
 export function ExploreCategory() {
-  return <section></section>;
+  return (
+    <section>
+      <ul>
+        <li>
+          <h2>Explore our wide variety of categories</h2>
+          <p>
+            Whether you're looking for a comedy to make you laugh, a drama to
+            make you think, or a documentary to learn something new
+          </p>
+        </li>
+        <li>
+          <button>
+            <img src="" alt="Left Arrow Icon" />
+          </button>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <button>
+            <img src="" alt="Right Arrow Icon" />
+          </button>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <img src="" alt="Movies Images" />
+          <figure>
+            <figcaption>Action</figcaption>
+            <img src="" alt="right arrow" />
+          </figure>
+        </li>
+      </ul>
+    </section>
+  );
 }
 
 export default ExploreCategory;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import { HomeHeader } from "./HomeHeader";
 
 export function HomePage() {
   return (
-    <main className="flex justify-center">
+    <main className="flex justify-center flex-col items-center">
       <HomeHeader />
       <ExploreCategory />
     </main>
