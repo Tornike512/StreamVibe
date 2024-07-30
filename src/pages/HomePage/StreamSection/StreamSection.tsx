41bc86fad361c23b3ee0ad66e3c8498f7e431c8f
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-30 21:49:12 +0400

    Optimize: Map Stream Backend

--- src/pages/HomePage/StreamSection/StreamSection.tsx
+++ src/pages/HomePage/StreamSection/StreamSection.tsx
@@ -1,6 +1,10 @@
+import { useGetStreams } from "src/Hooks/useGetStreams";
+
 import deviceIcon from "src/assets/Frame.png";
 
 export function StreamSection() {
+  const { streams } = useGetStreams();
+
   return (
     <section className="max-w-[1920px] w-full mb-[150px]">
       <h2 className="text-[#ffffff] text-[38px] font-bold mb-[14px]">
@@ -12,103 +16,31 @@ export function StreamSection() {
         range of devices, ensuring that you never miss a moment of
         entertainment.
       </p>
-      <div className="grid grid-cols-[1fr_1fr_1fr] gap-x-[30px] gap-y-[30px]">
-        <ul className="rounded-[12px] p-[50px] bg-[linear-gradient(to_right_top,#000000,#000000,#000000,#000000,#000000,#110609,#1c0d10,#241115,#3a171d,#501d24,#672428,#7e2b2b)]">
-          <li className="flex items-center mb-[30px]">
-            <img
-              className="bg-[#1F1F1F] px-[20px] py-[12px] rounded-[12px] mr-[16px]"
-              src={deviceIcon}
-              alt="Stream Device Image"
-            />
-            <h3 className="text-[#ffffff] text-[24px] font-bold">
-              Smartphones
-            </h3>
-          </li>
-          <p className="text-[18px] text-[#999999] mb-[80px]">
-            StreamVibe is optimized for both Android and iOS smartphones.
-            Download our app from the Google Play Store or the Apple App Store
-          </p>
-        </ul>
-        <ul className="rounded-[12px] p-[50px] bg-[linear-gradient(to_right_top,#000000,#000000,#000000,#000000,#000000,#110609,#1c0d10,#241115,#3a171d,#501d24,#672428,#7e2b2b)]">
-          <li className="flex items-center mb-[30px]">
-            <img
-              className="bg-[#1F1F1F] px-[20px] py-[12px] rounded-[12px] mr-[16px]"
-              src={deviceIcon}
-              alt="Stream Device Image"
-            />
-            <h3 className="text-[#ffffff] text-[24px] font-bold">
-              Smartphones
-            </h3>
-          </li>
-          <p className="text-[18px] text-[#999999] mb-[80px]">
-            StreamVibe is optimized for both Android and iOS smartphones.
-            Download our app from the Google Play Store or the Apple App Store
-          </p>
-        </ul>
-        <ul className="rounded-[12px] p-[50px] bg-[linear-gradient(to_right_top,#000000,#000000,#000000,#000000,#000000,#110609,#1c0d10,#241115,#3a171d,#501d24,#672428,#7e2b2b)]">
-          <li className="flex items-center mb-[30px]">
-            <img
-              className="bg-[#1F1F1F] px-[20px] py-[12px] rounded-[12px] mr-[16px]"
-              src={deviceIcon}
-              alt="Stream Device Image"
-            />
-            <h3 className="text-[#ffffff] text-[24px] font-bold">
-              Smartphones
-            </h3>
-          </li>
-          <p className="text-[18px] text-[#999999] mb-[80px]">
-            StreamVibe is optimized for both Android and iOS smartphones.
-            Download our app from the Google Play Store or the Apple App Store
-          </p>
-        </ul>
-        <ul className="rounded-[12px] p-[50px] bg-[linear-gradient(to_right_top,#000000,#000000,#000000,#000000,#000000,#110609,#1c0d10,#241115,#3a171d,#501d24,#672428,#7e2b2b)]">
-          <li className="flex items-center mb-[30px]">
-            <img
-              className="bg-[#1F1F1F] px-[20px] py-[12px] rounded-[12px] mr-[16px]"
-              src={deviceIcon}
-              alt="Stream Device Image"
-            />
-            <h3 className="text-[#ffffff] text-[24px] font-bold">
-              Smartphones
-            </h3>
-          </li>
-          <p className="text-[18px] text-[#999999] mb-[80px]">
-            StreamVibe is optimized for both Android and iOS smartphones.
-            Download our app from the Google Play Store or the Apple App Store
-          </p>
-        </ul>
-        <ul className="rounded-[12px] p-[50px] bg-[linear-gradient(to_right_top,#000000,#000000,#000000,#000000,#000000,#110609,#1c0d10,#241115,#3a171d,#501d24,#672428,#7e2b2b)]">
-          <li className="flex items-center mb-[30px]">
-            <img
-              className="bg-[#1F1F1F] px-[20px] py-[12px] rounded-[12px] mr-[16px]"
-              src={deviceIcon}
-              alt="Stream Device Image"
-            />
-            <h3 className="text-[#ffffff] text-[24px] font-bold">
-              Smartphones
-            </h3>
-          </li>
-          <p className="text-[18px] text-[#999999] mb-[80px]">
-            StreamVibe is optimized for both Android and iOS smartphones.
-            Download our app from the Google Play Store or the Apple App Store
-          </p>
-        </ul>
-        <ul className="rounded-[12px] p-[50px] bg-[linear-gradient(to_right_top,#000000,#000000,#000000,#000000,#000000,#110609,#1c0d10,#241115,#3a171d,#501d24,#672428,#7e2b2b)]">
-          <li className="flex items-center mb-[30px]">
-            <img
-              className="bg-[#1F1F1F] px-[20px] py-[12px] rounded-[12px] mr-[16px]"
-              src={deviceIcon}
-              alt="Stream Device Image"
-            />
-            <h3 className="text-[#ffffff] text-[24px] font-bold">
-              Smartphones
-            </h3>
-          </li>
-          <p className="text-[18px] text-[#999999] mb-[80px]">
-            StreamVibe is optimized for both Android and iOS smartphones.
-            Download our app from the Google Play Store or the Apple App Store
-          </p>
-        </ul>
+      <div className="grid grid-cols-[1fr_1fr_1fr] gap-x-[60px] gap-y-[60px]">
+        {streams.map((stream) => {
+          return (
+            <ul
+              key={stream.id}
+              className="rounded-[12px] p-[50px] bg-[linear-gradient(to_right_top,#000000,#000000,#000000,#000000,#000000,#110609,#1c0d10,#241115,#3a171d,#501d24,#672428,#7e2b2b)]"
+            >
+              <li className="flex items-center mb-[30px]">
+                <img
+                  className="bg-[#1F1F1F] px-[20px] py-[12px] rounded-[12px] mr-[16px]"
+                  src={stream.image}
+                  alt="Stream Device Image"
+                />
+                <h3 className="text-[#ffffff] text-[24px] font-bold">
+                  {stream.streamTitle}
+                </h3>
+              </li>
+              <p className="text-[18px] text-[#999999]">
+                StreamVibe is optimized for both Android and iOS smartphones.
+                Download our app from the Google Play Store or the Apple App
+                Store
+              </p>
+            </ul>
+          );
+        })}
       </div>
     </section>
   );
