a4de38deea887a8c8e28420f42dbe7edb71b0102
Tornike Tsagareishvili <dht5152@gmail.com>
2024-07-30 20:27:11 +0400

    Add: Stream Section HTML

--- src/pages/HomePage/StreamSection/StreamSection.tsx
+++ src/pages/HomePage/StreamSection/StreamSection.tsx
@@ -1,5 +1,25 @@
 export function StreamSection() {
-  return <section></section>;
+  return (
+    <section>
+      <h2>We Provide you streaming experience across various devices.</h2>
+      <p>
+        With StreamVibe, you can enjoy your favorite movies and TV shows
+        anytime, anywhere. Our platform is designed to be compatible with a wide
+        range of devices, ensuring that you never miss a moment of
+        entertainment.
+      </p>
+      <div>
+        <figure>
+          <img src="" alt="Stream Device Image" />
+          <figcaption>Smartphones</figcaption>
+        </figure>
+        <p>
+          StreamVibe is optimized for both Android and iOS smartphones. Download
+          our app from the Google Play Store or the Apple App Store
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default StreamSection;
