9d7bdaadb158a6c8650502ddbc02be60321f1f0e
Tornike Tsagareishvili <dht5152@gmail.com>
2024-08-02 22:58:49 +0400

    Add: FAQ Section HTML

--- src/pages/HomePage/FAQSection/FAQSection.tsx
+++ src/pages/HomePage/FAQSection/FAQSection.tsx
@@ -1,5 +1,24 @@
 export function FAQSection() {
-  return <section></section>;
+  return (
+    <section>
+      <div>
+        <div>
+          <h2>Frequently Asked Questions</h2>
+          <p>
+            Got questions? We've got answers! Check out our FAQ section to find
+            answers to the most common questions about StreamVibe.
+          </p>
+        </div>
+        <button>Ask a Question</button>
+      </div>
+      <ul>
+        <li>
+          <button>01</button>
+          <h3>What is StreamVibe?</h3>
+        </li>
+      </ul>
+    </section>
+  );
 }
 
 export default FAQSection;
