11554e3ce9d950f3a0804a89690bc10488399b2a
Tornike Tsagareishvili <dht5152@gmail.com>
2024-08-03 13:32:27 +0400

    Add: Plan Section HTML

--- src/pages/HomePage/PlanSection/PlanSection.tsx
+++ src/pages/HomePage/PlanSection/PlanSection.tsx
@@ -1,5 +1,36 @@
 export function PlanSection() {
-  return <section></section>;
+  return (
+    <section>
+      <div>
+        <div>
+          <h2>Choose the plan that's right for you</h2>
+          <p>
+            Join StreamVibe and select from our flexible subscription options
+            tailored to suit your viewing preferences. Get ready for non-stop
+            entertainment!
+          </p>
+        </div>
+        <div>
+          <button>Monthly</button>
+          <button>Yearly</button>
+        </div>
+      </div>
+      <ul>
+        <li>
+          <h3>Basic Plan</h3>
+          <p>
+            Enjoy an extensive library of movies and shows, featuring a range of
+            content, including recently released titles.
+          </p>
+          <h2>$9.99/Month</h2>
+          <div>
+            <button>Start Free Trial</button>
+            <button>Choose Plan</button>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
 }
 
 export default PlanSection;
