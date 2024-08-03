520d1de2c40c7d0036fb33799829763e78344c84
Tornike Tsagareishvili <dht5152@gmail.com>
2024-08-03 23:52:31 +0400

    Add: Footer HTML

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,61 @@
 export function Footer() {
-  return <footer></footer>;
+  return (
+    <>
+      {" "}
+      <footer>
+        <div>
+          <ul>
+            <h4>Home</h4>
+            <li>Categories</li>
+            <li>Devices</li>
+            <li>Pricing</li>
+            <li>FAQ</li>
+          </ul>
+          <ul>
+            <h4>Movies</h4>
+            <li>Gernes</li>
+            <li>Trending</li>
+            <li>New Release</li>
+            <li>Popular</li>
+          </ul>
+          <ul>
+            <h4>Shows</h4>
+            <li>Gernes</li>
+            <li>Trending</li>
+            <li>New Release</li>
+            <li>Popular</li>
+          </ul>
+          <ul>
+            <h4>Support</h4>
+            <li>Contact Us</li>
+          </ul>
+          <ul>
+            <h4>Subscription</h4>
+            <li>Plans</li>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>FAQ</li>
+          </ul>
+          <figure>
+            <figcaption>Connect With Us</figcaption>
+            <div>
+              <img src="" alt="Facebook Logo" />
+              <img src="" alt="Twitter Logo" />
+              <img src="" alt="LinkedIn Logo" />
+            </div>
+          </figure>
+        </div>
+      </footer>
+      <div>
+        <p>@Built By Tornike Tsagareishvili</p>
+        <div>
+          <p>Terms of Use</p>
+          <p>Privacy Policy</p>
+          <p>Cookie Policy</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
