04d3212c5f7c6742d64ffdd3943d28b7bf89b0f6
Tornike Tsagareishvili <dht5152@gmail.com>
2024-08-17 22:40:38 +0400

    Add: Start Plan Section Responsive For All Types Of Screens

--- src/pages/HomePage/StartPlanSection/StartPlanSection.tsx
+++ src/pages/HomePage/StartPlanSection/StartPlanSection.tsx
@@ -3,18 +3,22 @@ import planMainImage from "src/assets/plan-main-image.png";
 export function StartPlanSection() {
   return (
     <section className="flex justify-center relative w-full max-w-[1920px] mb-[150px] px-[40px]">
-      <img src={planMainImage} alt="Start Plan Main Image" />
-      <figure className="flex justify-around items-center w-full absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4">
+      <img
+        className="w-[-webkit-fill-available] lg:h-[400px] lg:object-cover"
+        src={planMainImage}
+        alt="Start Plan Main Image"
+      />
+      <figure className="flex justify-around items-center w-full absolute -translate-x-2/4 -translate-y-2/4 left-2/4 top-2/4 xl:w-[85%] lg:flex-col">
         <div className="">
-          <h1 className="text-[48px] text-[#ffffff] font-bold">
+          <h1 className="text-[48px] text-[#ffffff] font-bold xl:text-[32px] lg:justify-center lg:flex lg:items-center lg:text-center lg:mb-2.5">
             Start your free trial today!
           </h1>
-          <p className="text-[18px] text-[#999999] mb-[50px]">
+          <p className="text-[18px] text-[#999999] mb-[50px] lg:justify-center lg:flex lg:items-center lg:text-center">
             This is a clear and concise call to action that encourages users to
             sign up for a free trial of StreamVibe.
           </p>
         </div>
-        <button className="h-fit bg-[#E50000] text-[#ffffff] text-[18px] p-[20px] rounded-[8px] hover:bg-[#FF3333]">
+        <button className="h-fit bg-[#E50000] text-[#ffffff] whitespace-nowrap text-[18px] p-[20px] rounded-[8px] hover:bg-[#FF3333] xl:h-[66px] lg:w-[95%]">
           Start a Free Trail
         </button>
       </figure>
